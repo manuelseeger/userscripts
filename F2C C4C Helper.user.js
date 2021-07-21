46cd8145b341c159765bb9503c7d7f66b08bd7fc
Manuel Seeger <mail@manuelseeger.de>
2021-07-21 11:54:39 +0200

    Add C4C, ADO scripts

--- F2C C4C Helper.user.js
+++ F2C C4C Helper.user.js
@@ -4,15 +4,21 @@
 // @version      0.1
 // @description  Show A/UAT/PAV on C4C
 // @author       Manuel Seeger
-// @match        https://*.crm.ondemand.com
+// @match        https://*.crm.ondemand.com/*
 // @icon         https://www.google.com/s2/favicons?domain=ondemand.com
 // @grant        none
+// @require      https://gist.githubusercontent.com/BrockA/2625891/raw/9c97aa67ff9c5d56be34a55ad6c18a314e5eb548/waitForKeyElements.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // Your code here...
-
-    // new code
+    const main = (elems) => {
+        if (location.hostname.toString().includes('my343942')) {
+            elems[0].style.backgroundColor = 'aqua';
+        } else if (location.hostname.toString().includes('my350201')) {
+            elems[0].style.backgroundColor = 'red';
+        }
+    }
+    waitForKeyElements('#mainShell-header', main);
 })();
