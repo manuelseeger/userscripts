0bb8e244ce43bc8f40fd04d4825ec2c12f75b1b9
Manuel Seeger <mail@manuelseeger.de>
2021-07-21 09:37:04 +0200

    Add C4C helper

---
+++ F2C C4C Helper.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         F2C C4C Helper
+// @namespace    http://manuelseeger.de
+// @version      0.1
+// @description  Show A/UAT/PAV on C4C
+// @author       Manuel Seeger
+// @match        https://*.crm.ondemand.com
+// @icon         https://www.google.com/s2/favicons?domain=ondemand.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+
+    // new code
+})();
